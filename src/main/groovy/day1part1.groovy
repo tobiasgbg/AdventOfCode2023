9bdc1880717cc9e646ba55507610274ac5324405
Tobias Adriansson <tobias.gbg@gmail.com>
2023-12-06 06:01:07 +0100

    Solve day 1

--- src/main/groovy/day1part1.groovy
+++ src/main/groovy/day1part1.groovy
@@ -22,27 +22,70 @@ In this example, the calibration values of these four lines are 12, 38, 15, and 
 
 Consider your entire calibration document. What is the sum of all of the calibration values?
 
+--- Part Two ---
+Your calculation isn't quite right. It looks like some of the digits are actually spelled out with letters: one, two, three, four, five, six, seven, eight, and nine also count as valid "digits".
+
+Equipped with this new information, you now need to find the real first and last digit on each line. For example:
+
+two1nine
+eightwothree
+abcone2threexyz
+xtwone3four
+4nineeightseven2
+zoneight234
+7pqrstsixteen
+In this example, the calibration values are 29, 83, 13, 24, 42, 14, and 76. Adding these together produces 281.
+
+What is the sum of all of the calibration values?
+
  */
 
 static void main(String[] args) {
 
     List<String> fileContents = new File('../../../input/day1.txt').text.split('\n')
 
-    def totalValue = Day1Part1Trebuchet.sumCalibrationValues(fileContents)
+    def totalValue = Day1Trebuchet.sumCalibrationValues(fileContents)
     println("TotalValue ${totalValue}")
 }
 
-class Day1Part1Trebuchet {
+class Day1Trebuchet {
+
     static Integer sumCalibrationValues(List<String> document) {
         document.sum { line ->
             // Extract the first digit
-            def firstDigit = line.find { it.isNumber() }
-
-            // Extract the last digit (search from the end)
-            def lastDigit = line.reverse().find { it.isNumber() }
+            def digits = findDigits(line)
 
             // Combine the digits to form a number and convert to integer
-            return (firstDigit + lastDigit) as Integer
+            return (digits.first().toString() + digits.last().toString()) as Integer
         } as Integer
+    }
+
+    static List<String> findDigits(String str) {
+        def result = []
+        for (int i = 0; i < str.length(); i++) {
+            for (int j = i + 1; j <= str.length(); j++) {
+                String substring = str.substring(i, j)
+                def digit = getDigit(substring)
+                if (digit) {
+                    result.add(digit)
+                }
+            }
+        }
+
+        result
+    }
+
+    static Integer getDigit(String word) {
+
+        def wordToDigitMap = [
+                'one': 1, 'two': 2, 'three': 3, 'four': 4, 'five': 5,
+                'six': 6, 'seven': 7, 'eight': 8, 'nine': 9
+        ]
+        def result = null
+        if (wordToDigitMap.containsKey(word.toLowerCase()))
+            return wordToDigitMap.get(word.toLowerCase()) as Integer
+        else if (word.isNumber())
+            return word as Integer
+        result
     }
 }
--- src/test/groovy/Day1Specification.groovy
+++ src/test/groovy/Day1Specification.groovy
@@ -7,6 +7,14 @@ class Day1Specification extends Specification {
         def document = ["a1b1", "c2d2"]
 
         expect:
-        Day1Part1Trebuchet.sumCalibrationValues(document) == 33
+        Day1Trebuchet.sumCalibrationValues(document) == 33
+    }
+
+    def "sum calibration values for document with same first and last digit"() {
+        given:
+        def document = ["two1nine", "eightwothree"]
+
+        expect:
+        Day1Trebuchet.sumCalibrationValues(document) == 112
     }
 }
