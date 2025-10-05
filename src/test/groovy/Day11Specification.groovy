20c6f388780f2a808a37f112732a1fd04f2c5470
Tobias Adriansson <tobias.gbg@gmail.com>
2025-10-05 15:28:38 +0200

    Add day 11

---
+++ src/test/groovy/Day11Specification.groovy
@@ -0,0 +1,23 @@
+import spock.lang.Specification
+
+class Day11Specification extends Specification {
+
+    def "example test 1"() {
+        given:
+        String input = """...#......
+.......#..
+#.........
+..........
+......#...
+.#........
+.........#
+..........
+.......#..
+#...#....."""
+
+        // TODO: Implement test
+
+        expect:
+        true // Replace with actual test
+    }
+}
