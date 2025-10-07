f8b8aae603bdf0b4595e92914c62fc0c853bcfc2
Tobias Adriansson <tobias.gbg@gmail.com>
2025-10-07 20:34:26 +0200

    day 12 boiler plate

---
+++ src/test/groovy/Day12Specification.groovy
@@ -0,0 +1,19 @@
+import spock.lang.Specification
+
+class Day12Specification extends Specification {
+
+    static final String EXAMPLE_INPUT = """???.### 1,1,3
+.??..??...?##. 1,1,3
+?#?#?#?#?#?#?#? 1,3,1,6
+????.#...#... 4,1,1
+????.######..#####. 1,6,5
+?###???????? 3,2,1"""
+
+    def "example test - sum of arrangement counts is 21"() {
+        given:
+        // TODO: Implement test
+
+        expect:
+        true // Replace with actual test when implemented
+    }
+}
